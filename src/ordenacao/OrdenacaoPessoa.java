a4441f0bd9cab1932a43ba61b958464fc0725613
guilherdrk <guibem20@gmail,com>
2025-05-28 22:44:37 -0300

    Ordenação List

---
+++ src/ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,37 @@
+package ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa(){
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+    public List<Pessoa> ordenadoPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparetorPorAltura());
+        return pessoasPorAltura;
+    }
+
+}
+class ComparetorPorAltura implements Comparator<Pessoa>{
+
+    @Override
+    public int compare(Pessoa o1, Pessoa o2) {
+        return Double.compare(o1.getAltura(), o2.getAltura());
+    }
+}
